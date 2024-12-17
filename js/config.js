6a3c506b1b5b61fd052db70978313069832672ad
Magikh <hoor.k3v@gmail.com>
2024-12-17 13:47:43 +0100

    Initial commit

---
+++ js/config.js
@@ -0,0 +1,17 @@
+const firstBtn = document.getElementById("firstBtn")
+const secondBtn = document.getElementById("secondBtn")
+const thirdBtn = document.getElementById("thirdBtn")
+
+const miniFirstButton = document.getElementById("miniFirstButton")
+const miniSecondButton = document.getElementById("miniSecondButton")
+
+// Here you can change the content of the buttons
+
+// Here are the first three buttons
+firstBtn.innerHTML = "Singleplayerr"
+secondBtn.innerHTML = "Multiplayer"
+thirdBtn.innerHTML = "Minecraft Realms"
+
+// Here are the two small buttons
+miniFirstButton.innerHTML = "Options..."
+miniSecondButton.innerHTML = "Quit Game"
